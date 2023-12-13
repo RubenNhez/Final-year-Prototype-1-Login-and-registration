f56f9fbf911d1dcf1351945cac6527dfd8235a6c
RubenNhez <ruben.nhez@gmail.com>
2023-12-13 00:06:02 +0000

    database connected

--- routes/db-config.js
+++ routes/db-config.js
@@ -1,9 +1,10 @@
 const sql = require("mysql")
+const dotenv = require("dotenv").config();
 const db = sql.createConnection({
     host:process.env.DATABASE_HOST,
     user:process.env.DATABASE_USER,
     password:process.env.DATABASE_PASSWORD,
-    database:process.env.DATABASE_HOST
+    database:process.env.DATABASE_DB
 })
 
 module.exports = db;
